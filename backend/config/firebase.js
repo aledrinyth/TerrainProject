62b63e224b74d502a65ab8febf401386654192d3
professionalvictim <167693760+professionalvictim@users.noreply.github.com>
2025-09-11 22:25:14 +1000

    UNTESTED: Completed login API, created basic dockerfiles and docker-compose, created empty files to be filled out in the future

---
+++ backend/config/firebase.js
@@ -0,0 +1,6 @@
+// import firebase-app and firebase-firestore packages when we get firebase
+
+
+var config; // All this copy paste from the firebase when we get it (put API key in an env and use dotenv)
+
+const db = firebase.firestore();
