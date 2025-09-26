fa05de458152025531d9b314aeede8bd1a574a79
professionalvictim <167693760+professionalvictim@users.noreply.github.com>
2025-09-26 19:39:48 +1000

    Remove unneeded routes

--- terrainapp/backend/server.js
+++ terrainapp/backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 
 // Import router files
-const registerRoutes = require('./routers/register-router');
 const deskRoutes = require('./routers/desk-router');
 const bookingRoutes = require('./routers/booking-router');
 const userRoutes = require('./routers/user-router');
@@ -22,7 +21,6 @@ app.get('/api/health', (req, res) => {
 // Middleware
 app.use(cors()); // Enable CORS for frontend communication
 app.use(express.json()); // Checks for JSON body in requests
-app.use('/api/register', registerRoutes);
 app.use('/api/desk', deskRoutes);
 app.use('/api/booking', bookingRoutes);
 app.use('/api/user', userRoutes);
